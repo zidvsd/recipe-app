560f52dd461b99175c3570e87f034c400aa23aca
Zid Visda <rashidvisda@gmail.com>
2025-04-17 22:14:02 +0800

    Fixed image path

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import logo from "../assets/images/favicon.png";
 const Logo = () => {
   return (
     <div className="flex flex-row gap-x-2 items-center">
-      <img src="/src/assets/images/favicon.png" className="size-12" alt="" />
+      <img src={logo} className="size-12" alt="" />
       <h1 className="select-none heading-text text-3xl text-honey-yellow cursor-pointer">
         Spoon<span className="text-custom-peach">ful</span>
       </h1>
