fa19e103c55639b62678e4e1124108a4446a80c8
Zid Visda <rashidvisda@gmail.com>
2025-04-26 17:23:50 +0800

    Added single page recipe

---
+++ src/components/PostMeta.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import profilePlaceholder from "../assets/images/thumbnails/profile-placeholder.jpeg";
+import { CalendarFold } from "lucide-react";
+import { Star } from "lucide-react";
+
+const PostMeta = ({ selectedItem }) => {
+  return (
+    <>
+      {" "}
+      <h1 className="heading-text heading-size">
+        {selectedItem.title || selectedItem.strMeal}
+      </h1>
+      <section className="mt-4 flex pb-4 flex-row gap-x-6 gap-y-2 items-center justify-start mb-4 flex-wrap border-b border-neutral-300">
+        <div className="flex flex-row gap-x-2 items-center">
+          <img
+            src={profilePlaceholder}
+            className="rounded-full size-8"
+            alt=""
+          />
+          <p className="subheading-text">Mang Juan</p>
+        </div>
+        <div className="flex flex-row gap-x-2 items-center ">
+          <CalendarFold />
+          <p className="subheading-text">{selectedItem.date || "N/A Date"}</p>
+        </div>
+        <div className="flex flex-row gap-x-1 items-center">
+          <p className="subheading-text">5.0 Reviews</p>
+          {[...Array(5)].map((_, i) => (
+            <Star
+              key={i}
+              className="w-4 h-4 text-honey-yellow fill-honey-yellow"
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default PostMeta;
--- src/pages/SingleRecipePage.jsx
+++ src/pages/SingleRecipePage.jsx
@@ -2,7 +2,12 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { RecipeContext } from "../context/RecipeProvider";
 import { slugify } from "../utils/urlTextHandler";
+import FadeInWhenVisible from "../components/animations/FadeInWhenVisible.jsx";
+import BlogSidebar from "../components/BlogSidebar.jsx";
+import RecipeSidebar from "../components/RecipeSidebar.jsx";
+import PostMeta from "../components/PostMeta.jsx";
 const SingleRecipePage = () => {
+  const { slug } = useParams();
   const { meals, loading, error } = useContext(RecipeContext);
   const { id } = useParams();
 
@@ -15,15 +20,23 @@ const SingleRecipePage = () => {
   if (!selectedRecipe) return <p>Recipe not found.</p>;
 
   return (
-    <div className="custom-container mx-auto">
-      <h1 className="text-2xl font-bold">{selectedRecipe.strMeal}</h1>
-      <img
-        src={selectedRecipe.strMealThumb}
-        alt={selectedRecipe.strMeal}
-        className="w-full max-w-md rounded-lg mt-4"
-      />
-      <p className="mt-4">{selectedRecipe.strInstructions}</p>
-    </div>
+    <FadeInWhenVisible>
+      <section className="custom-container mx-auto space-y-4  flex flex-col md:flex-row gap-x-4">
+        <div className="w-full md:w-[70%] lg:w-[75%]">
+          <PostMeta selectedItem={selectedRecipe} />
+          <img
+            src={selectedRecipe.strMealThumb}
+            alt={selectedRecipe.strMeal}
+            className="w-full rounded-lg  max-h-[42rem] object-cover"
+          />
+          <p className="mt-4">{selectedRecipe.strInstructions}</p>
+        </div>
+        <div className="flex-col space-y-4  w-full md:w-[30%] lg:wd-[25%]">
+          <RecipeSidebar currentSlug={slug} />
+          <BlogSidebar currentSlug={slug} />
+        </div>
+      </section>
+    </FadeInWhenVisible>
   );
 };
 
