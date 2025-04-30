93b694c82f7a3705a73e5273e8682fbd3de2dcf4
Zid Visda <rashidvisda@gmail.com>
2025-04-30 19:52:01 +0800

    Added lazy loading

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,10 +4,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import BreadCrumbs from "../components/BreadCrumbs";
 import Blob from "../components/Blob";
+import { useLocation } from "react-router-dom";
 const MainLayout = ({ children }) => {
+  let location = useLocation();
+
   return (
     <>
-      <Blob />
+      {location.pathname !== "/" ? null : <Blob />}
       <Navbar />
       <BreadCrumbs />
       <main className=" :mt-0 space-y-8 sm:space-y-12 md:space-y-16  ">
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,42 +2,45 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import AuthLayout from "../layouts/AuthLayout.jsx";
-import {
-  AboutPage,
-  BlogPage,
-  HomePage,
-  LoginPage,
-  CategoryPage,
-  RecipePage,
-  SignupPage,
-  SingleBlogPage,
-  SingleRecipePage,
-} from "../pages/index.js";
+import { lazy, Suspense } from "react";
 import NotFound from "../components/NotFound";
-import SingleCategoryPage from "../pages/SingleCategoryPage.jsx";
-
+import { h1 } from "framer-motion/client";
+const AboutPage = lazy(() => import("../pages/AboutPage.jsx"));
+const BlogPage = lazy(() => import("../pages/BlogPage.jsx"));
+const HomePage = lazy(() => import("../pages/HomePage.jsx"));
+const LoginPage = lazy(() => import("../pages/LoginPage.jsx"));
+const SignupPage = lazy(() => import("../pages/SignupPage.jsx"));
+const RecipePage = lazy(() => import("../pages/RecipePage.jsx"));
+const CategoryPage = lazy(() => import("../pages/CategoryPage.jsx"));
+const SingleRecipePage = lazy(() => import("../pages/SingleRecipePage.jsx"));
+const SingleBlogPage = lazy(() => import("../pages/SingleBlogPage.jsx"));
+const SingleCategoryPage = lazy(
+  () => import("../pages/SingleCategoryPage.jsx"),
+);
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="recipe" element={<RecipePage />} />
-            <Route path="recipe/:id" element={<SingleRecipePage />} />
-            <Route path="blog/:slug" element={<SingleBlogPage />} />
-            <Route path="blog" element={<BlogPage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="category" element={<CategoryPage />} />
-            <Route path="category/:id" element={<SingleCategoryPage />} />
+      <Suspense fallback={<h1>loading....</h1>}>
+        <Routes>
+          <Route>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="recipe" element={<RecipePage />} />
+              <Route path="recipe/:id" element={<SingleRecipePage />} />
+              <Route path="blog/:slug" element={<SingleBlogPage />} />
+              <Route path="blog" element={<BlogPage />} />
+              <Route path="about" element={<AboutPage />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="category" element={<CategoryPage />} />
+              <Route path="category/:id" element={<SingleCategoryPage />} />
+            </Route>
+            <Route element={<AuthLayout />}>
+              <Route path="signin" element={<SignupPage />} />
+              <Route path="login" element={<LoginPage />} />
+            </Route>
           </Route>
-          <Route element={<AuthLayout />}>
-            <Route path="signin" element={<SignupPage />} />
-            <Route path="login" element={<LoginPage />} />
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
